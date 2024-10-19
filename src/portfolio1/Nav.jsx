8f4b1cc9ca745d12f76120a09277798599d383bd
Motunrayo780 <bakaremotunrayo818@gmail.com>
2024-10-18 22:45:14 -0700

    set up portfolio project

--- src/portfolio1/Nav.jsx
+++ src/portfolio1/Nav.jsx
@@ -7,7 +7,7 @@ const Nav =() =>  {
       {/* Navbar */}
       <header className="bg-blue-800 text-white p-4 fixed w-full top-0 shadow-lg z-10">
         <nav className="flex justify-between items-center max-w-7xl mx-auto">
-          <div className="text-3xl font-bold">Your Name</div>
+          <div className="text-3xl font-bold">MOTUNRAYO</div>
           <div className="space-x-4">
             <a href="#about" className="hover:text-fuchsia-200">About</a>
             <a href="#skills" className="hover:text-fuchsia-200">Skills</a>
